32ff5f5af95ba501ee9b3168efaac6bcf6ab0ec3
Victor Vazquez <victor.vamv17@gmail.com>
2023-06-19 19:55:21 -0600

    add:Ejercicio FizzBuzz resuelto

--- FizzBuzz/Prueba1.js
+++ FizzBuzz/Prueba1.js
@@ -1,16 +1,35 @@
-let x = 0
 
-let listaNumeros = new Array(10)
-for (let index = 0; index < listaNumeros.length; index++) {
-    let suma = listaNumeros[index];  
-    x = x + 1
-    suma = x
-    listaNumeros[index]= suma
-    console.log(suma)  
-    listaNumeros[2] = "Fizz"
+const prompt = require('prompt-sync')();
+
+const entrada = prompt('Ingrese un número entero: ');
+const numeroUsuario = parseInt(entrada);
+
+function FizzBuzz(numero){
+
+let x = 0
+let listaNumeros = new Array(numero)
+    
+    for (let index = 0; index < listaNumeros.length; index++) { 
+        x = x + 1
+        let suma = x 
+        listaNumeros[index]= suma
+        
+        let modulo = listaNumeros[index] % 3
+        let modulo2 = listaNumeros[index] % 5
+        if (modulo == 0 && modulo2 == 0) {
+            listaNumeros[index] = "FizzBuzz" 
+        } else if (modulo2 == 0) {
+            listaNumeros[index] = "Buzz"
+        } else if(modulo == 0) {
+            listaNumeros[index] = "Fizz"
+        }
+        
+    }
+    console.log(listaNumeros)
 }
 
-console.log(listaNumeros)
+FizzBuzz(numeroUsuario)
+
 
 
 
